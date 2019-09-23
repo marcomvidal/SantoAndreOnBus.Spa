7d8786ee63a3fbc78be45ca009f3a6f611ef2319
Marco M. Vidal <marcomvidal@gmail.com>
2019-09-23 15:47:09 -0300

    Initial commit

---
+++ src/app/navbar/Link.ts
@@ -0,0 +1,6 @@
+export class Link {
+    id: number;
+    icon: string;
+    label: string;
+    route: string;
+}
---
+++ src/app/navbar/links-data.ts
@@ -0,0 +1,7 @@
+import {Link} from './Link';
+
+export const LINKS: Link[] = [
+    { id: 1, label: 'Linhas',   icon: 'fas fa-tags',        route: '#1' },
+    { id: 2, label: 'Empresas', icon: 'fas fa-building',    route: '#2' },
+    { id: 3, label: 'Veículos', icon: 'fas fa-bus',         route: '#3' }
+];
